d8bb5c10f0fe2ac49fe57d6a8ef9272db2f8b382
Rorrian <nkunegina@yandex.ru>
2025-02-12 13:06:42 +0100

    refactored

--- src/shared/components/AuthForm/AuthForm.tsx
+++ src/shared/components/AuthForm/AuthForm.tsx
@@ -1,5 +1,17 @@
-import { useState } from 'react'
-import { Button, Input, Card, CardBody, CardFooter } from '@nextui-org/react'
+import {
+  Controller,
+  FieldValues,
+  SubmitHandler,
+  useForm,
+} from 'react-hook-form'
+import {
+  Button,
+  Input,
+  Card,
+  CardBody,
+  CardFooter,
+  Spinner,
+} from '@nextui-org/react'
 import { MdOutlineMail } from 'react-icons/md'
 
 import { AuthCredentials } from '@/src/types'
@@ -10,45 +22,84 @@ interface AuthFormProps {
 }
 
 export const AuthForm = ({ title, handleClick }: AuthFormProps) => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm()
+
+  const handleSubmitClick: SubmitHandler<FieldValues> = data => {
+    const credentials: AuthCredentials = {
+      email: data.email,
+      password: data.password,
+    }
+
+    handleClick(credentials)
+  }
 
   return (
     <Card className="w-96 shadow-lg bg-white rounded-3xl">
       <CardBody className="flex flex-col gap-4">
-        <Input
-          fullWidth
-          isClearable
-          label="Email"
-          placeholder="Введите ваш email"
-          type="email"
-          value={email}
-          variant="bordered"
-          startContent={
-            <MdOutlineMail className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-          }
-          onChange={e => setEmail(e.target.value)}
-          onClear={() => setEmail('')}
+        <Controller
+          name="email"
+          control={control}
+          defaultValue={''}
+          render={({ field }) => (
+            <Input
+              {...field}
+              isRequired
+              className="w-full"
+              isClearable
+              fullWidth
+              errorMessage={({ validationDetails, validationErrors }) => {
+                if (validationDetails.typeMismatch) {
+                  return 'Введите корректный email'
+                }
+
+                return validationErrors
+              }}
+              label="Email"
+              placeholder="Введите ваш email"
+              type="email"
+              variant="bordered"
+              startContent={
+                <MdOutlineMail className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+              }
+              onClear={() => reset({ email: '' })}
+            />
+          )}
         />
 
-        <Input
-          fullWidth
-          isClearable
-          label="Пароль"
-          placeholder="Введите ваш пароль"
-          type="password"
-          value={password}
-          variant="bordered"
-          onChange={e => setPassword(e.target.value)}
-          onClear={() => setEmail('')}
+        <Controller
+          name="password"
+          control={control}
+          rules={{ required: 'Пароль обязателен' }}
+          defaultValue=""
+          render={({ field }) => (
+            <Input
+              {...field}
+              isRequired
+              className="w-full"
+              errorMessage={errors.password?.message}
+              isClearable
+              isInvalid={!!errors.password?.message}
+              fullWidth
+              label="Пароль"
+              placeholder="Введите ваш пароль"
+              type="password"
+              variant="bordered"
+              onClear={() => reset({ password: '' })}
+            />
+          )}
         />
 
         <CardFooter className="flex justify-center">
           <Button
-            color="primary"
             autoFocus
-            onPress={() => handleClick({ email, password })}
             className="w-full py-2 text-lg font-medium bg-gradient-to-r from-[#006D77] via-[#83C5BE] to-[#EDF6F9] hover:from-[#006D77] hover:via-[#83C5BE] hover:to-[#E29578]"
+            color="primary"
+            onClick={handleSubmit(handleSubmitClick)}
           >
             {title}
           </Button>
